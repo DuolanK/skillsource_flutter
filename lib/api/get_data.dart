b2b5d3e9a61fe4ff6720f873b5c5d8e3a822b24e
DuolanK <duolan.kharitonov@mail.ru>
2023-06-16 19:19:28 +0900

    16 june commit

--- lib/api/get_data.dart
+++ lib/api/get_data.dart
@@ -1,52 +1,43 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
 
-class GetDataPage extends StatefulWidget {
-  @override
-  _GetDataPageState createState() => _GetDataPageState();
+import 'package:http/http.dart' as http;
+
+Future<void> fetchData() async {
+  final apiUrl = 'http://127.0.0.1:8000';
+
+  final quizDataResponse = await http.get(Uri.parse('$apiUrl/quiz/'));
+  final questionDataResponse = await http.get(Uri.parse('$apiUrl/question/'));
+  final rightAnswerDataResponse =
+      await http.get(Uri.parse('$apiUrl/rightanswer/'));
+  final wrongAnswerDataResponse =
+      await http.get(Uri.parse('$apiUrl/wronganswer/'));
+  final coursesDataResponse = await http.get(Uri.parse('$apiUrl/courses/'));
+
+  if (quizDataResponse.statusCode == 200 &&
+      questionDataResponse.statusCode == 200 &&
+      rightAnswerDataResponse.statusCode == 200 &&
+      wrongAnswerDataResponse.statusCode == 200 &&
+      coursesDataResponse.statusCode == 200) {
+    final quizData = quizDataResponse.body;
+    final questionData = questionDataResponse.body;
+    final rightAnswerData = rightAnswerDataResponse.body;
+    final wrongAnswerData = wrongAnswerDataResponse.body;
+    final coursesData = coursesDataResponse.body;
+
+    await saveDataToFile(quizData, 'quiz.json');
+    await saveDataToFile(questionData, 'question.json');
+    await saveDataToFile(rightAnswerData, 'rightanswer.json');
+    await saveDataToFile(wrongAnswerData, 'wronganswer.json');
+    await saveDataToFile(coursesData, 'courses.json');
+  } else {
+    print('Failed to fetch data. Status code: ${quizDataResponse.statusCode}');
+  }
 }
 
-class _GetDataPageState extends State<GetDataPage> {
-  final String apiUrl = 'http://127.0.0.1:8000/quiz/';
-
-  @override
-  void initState() {
-    super.initState();
-    fetchDataAndSaveToFile();
-  }
-
-  Future<void> fetchDataAndSaveToFile() async {
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-
-      if (response.statusCode == 200) {
-        final directory = await getApplicationDocumentsDirectory();
-        final filePath = '${directory.path}/assets/data/quiz.json';
-
-        final file = File(filePath);
-        await file.writeAsString(response.body);
-
-        print('Данные успешно сохранены в файл: $filePath');
-      } else {
-        print('Ошибка при получении данных: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Ошибка при выполнении запроса: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('API Example'),
-      ),
-      body: Center(
-        child: Text('Данные загружаются при запуске приложения'),
-      ),
-    );
-  }
+Future<void> saveDataToFile(String data, String fileName) async {
+  final directory = Directory('packages/skillsource/assets/data');
+  final file = File('${directory.path}/$fileName');
+  await file.writeAsString(data);
+  print('Data saved to ${file.path}');
 }
