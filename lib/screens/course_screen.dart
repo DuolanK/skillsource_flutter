cc16018e3721a2b2b704b2ffa0a2566dbf921c29
DuolanK <duolan.kharitonov@mail.ru>
2023-06-21 09:54:00 +0900

    json view

--- lib/screens/course_screen.dart
+++ lib/screens/course_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'quiz_screen.dart';
 import 'theory_screen.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class CourseScreen extends StatefulWidget {
   @override
@@ -12,65 +14,21 @@ class CourseScreen extends StatefulWidget {
 class _CourseScreenState extends State<CourseScreen> {
   Future<List<Map<String, dynamic>>>? _courseDataFuture;
   Future<List<Map<String, dynamic>>> getCourseData() async {
-    // Load all three JSON files
-    String jsonStringCourses = jsonEncode([
-      {
-        "type": "quiz",
-        "title": "I. Базовые знания",
-        "description": "Базовые знания сварочного дела",
-        "isAccessible": true,
-        "quizId": 1
-      },
-      {
-        "type": "quiz",
-        "title": "II. Материалы для сварки",
-        "description": "Вопросы по материалам",
-        "isAccessible": true,
-        "quizId": 2
-      },
-      {
-        "type": "quiz",
-        "title": "III. Оборудование для сварки",
-        "description": "Вопросы по материалам",
-        "isAccessible": true,
-        "quizId": 3
-      },
-      {
-        "type": "quiz",
-        "title": "IV. Основы безопасности на рабочем месте",
-        "description": "Вопросы по материалам",
-        "isAccessible": true,
-        "quizId": 4
-      },
-      {
-        "type": "quiz",
-        "title": "V. Технологии сварки",
-        "description": "Вопросы по материалам",
-        "isAccessible": true,
-        "quizId": 5
-      },
-      {
-        "type": "quiz",
-        "title": "VI. Контроль качества сварного шва",
-        "description": "Вопросы по материалам",
-        "isAccessible": true,
-        "quizId": 6
-      }
-    ]);
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/courses.json';
+    String jsonStringCourses = await File(filePath).readAsString();
 
     String jsonStringQuizzes =
         await rootBundle.loadString('lib/assets/data/quiz.json');
     String jsonStringTheoryMaterials =
         await rootBundle.loadString('lib/assets/data/theory_materials.json');
 
-    // Convert JSON strings to lists with Map objects
     List<Map<String, dynamic>> jsonDataCourses =
         List<Map<String, dynamic>>.from(json.decode(jsonStringCourses));
     List<dynamic> jsonDataQuizzes = json.decode(jsonStringQuizzes);
     List<Map<String, dynamic>> jsonDataTheoryMaterials =
         List<Map<String, dynamic>>.from(json.decode(jsonStringTheoryMaterials));
 
-    // Create a new list of Maps with the correct format
     List<Map<String, dynamic>> courseData = [];
 
     for (var courseItem in jsonDataCourses) {
@@ -156,7 +114,6 @@ class _CourseScreenState extends State<CourseScreen> {
                         );
                       }
                     } else {
-                      // Display error message or blocked access
                       ScaffoldMessenger.of(context).showSnackBar(
                         SnackBar(
                           content: Text('Access denied or data not found'),
