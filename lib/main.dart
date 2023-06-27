f173b26a093038b2c1a369a42a9b36711a5eb50f
DuolanK <duolan.kharitonov@mail.ru>
2023-06-28 04:37:33 +0900

    28june

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'QuizApp',
+      title: 'SkillsourseApp',
       initialRoute: '/myapp',
       routes: {
         '/dashboard': (context) => DashboardScreen(),
